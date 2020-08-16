7aca6431514e30f164bde854a6d96c98f470fb38
Liberbell <liberbell@gmail.com>
2020-08-17 08:51:16 +0900

    printarray11

--- printarray.py
+++ printarray.py
@@ -12,5 +12,5 @@ print(z)
 print(np.arange(12100))
 print(np.arange(12100).reshape(110, 110))
 
-np.set_printoptions(threshold=np.non-NAN)
+np.set_printoptions(threshold=non-NAN)
 print(np.arange(12100).reshape(110, 110))

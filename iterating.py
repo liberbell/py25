2229bf2b45af4c7d291a32828a57afccfc2a0c01
Liberbell <liberbell@gmail.com>
2020-08-23 10:46:35 +0900

    iterating6

--- iterating.py
+++ iterating.py
@@ -6,7 +6,7 @@ print(x)
 for i in x:
     print(i)
 
-companies = ([["Capcon", "Microsoft", "IBM", "Spotify", "Filipkart"],
+companies = np.array([["Capcon", "Microsoft", "IBM", "Spotify", "Filipkart"],
              [1938, 1975, 1911, 2006, 2007],
              [489000, 1310000, 380000, 3000, 30000]])
 
@@ -14,3 +14,5 @@ i = 0
 for row in companies:
     print("Row", i, ": ", row)
     i += 1
+
+print(companies.flatten())

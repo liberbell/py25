cf079db7158c6d57c242bf257745a9a9044bb887
Liberbell <liberbell@gmail.com>
2020-08-24 08:54:01 +0900

    changeshpe2

---
+++ changeshape.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+tech_companies = np.array(["IBM", "Apple.inc", "Intel", "Dell", "Microsoft"],
+                          ["New York", "California", "California", "Texas", "Washinton"])
+
+print(tech_companies)

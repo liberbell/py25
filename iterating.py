889a019085ccd4381b8daa8df96f4447c715f5c2
Liberbell <liberbell@gmail.com>
2020-08-23 10:41:38 +0900

    iterating4

--- iterating.py
+++ iterating.py
@@ -5,3 +5,7 @@ print(x)
 
 for i in x:
     print(i)
+
+companies = ([["Capcon", "Microsoft", "IBM", "Spotify", "Filipkart"],
+             [1938, 1975, 1911, 2006, 2007],
+             [489000, 1310000, 380000, 3000, 30000]])

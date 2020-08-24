2715b72cc31bd8fa60c9e609cf5e3f57a301d1ad
Liberbell <liberbell@gmail.com>
2020-08-25 08:54:33 +0900

    changeshape5

--- changeshape.py
+++ changeshape.py
@@ -13,3 +13,6 @@ print(tech_companies.shape)
 
 print(tech_companies.reshape(2, 5))
 print(np.arange(18).reshape(3, 6))
+
+companies = np.array(["IBM", "Apple", "Intel", "Sony", "Microsoft", "HP", "Hitachi", "Panasonic"])
+print(companies)
